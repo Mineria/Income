a94e29e4ca93bcea96979f8a1ee43ea85bde1512
Jorge Ferreiro <me@jgferreiro.com>
2016-04-07 15:40:09 +0200

    Added methods to work with pandas

--- adults.py
+++ adults.py
@@ -1,6 +1,56 @@
-import csv as csv
+# -*- coding: utf-8 -*-
+
 import pandas as pd
+import numpy as np
+import pylab as P
 
-train_matrix = pd.read_csv('Data/data.csv', header=0)        # Load the train file into a dataframe
+# Creating dataframe with CSV file
+df = pd.read_csv('Data/data.csv', header=0);
 
-print train_matrix
+# Normalising non-continuous parameters (given in string)
+# df['workclass'] = df['workclass'].map( lambda x: workclass(x) ).astype(int)
+# df['education'] = df['education'].map( lambda x: education(x) ).astype(int)
+# df['marital-status'] = df['marital-status'].map( lambda x: marital_status(x) ).astype(int)
+# df['occupation'] = df['occupation'].map( lambda x: occupation(x) ).astype(int)
+# df['relationship'] = df['relationship'].map( lambda x: relationship(x) ).astype(int)
+# df['race'] = df['race'].map( lambda x: race(x) ).astype(int)
+# df['sex'] = df['sex'].map( {'Female': 0, 'Male': 1} ).astype(int)
+# df['native-country'] = df['native-country'].map( lambda x: native_country(x) ).astype(int)
+
+# Check the our dataframse is only containing numbers
+df.dtypes[df.dtypes.map(lambda x: x=='object')]
+#df['sex'] = df['sex'].map( lambda x: sex(x) ).astype(int)
+
+# Delete unused columns
+#df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+# Delete first column refering to user index
+df = df.drop(df.columns[0], axis=1)
+
+train_data = df.values
+print train_data[2]
+
+
+#
+# # Get information
+# print df.info()
+# #print df.describe()
+# # Get columns type
+# #print df.dtypes
+#
+# # Filter columns
+# #print df[(df['income'] == '>50K') & (df['sex'] == 'female')][['age']]
+# print df[df['income'].isnull()][['age', 'sex', 'race']]
+#
+# df['age'].hist()
+# P.show()
+#
+#
+# # Display specific columns
+# #print df[ ['age', 'workclass']]
+#
+# #
+# # print type(df['age'])
+# #
+# # for age in df['age'][0:10]:
+# #     print age
+# # print df['age'][0:10]
