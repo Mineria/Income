782b7990ab57c576cc0396405129c30b48d249f0
Jorge Ferreiro <me@jgferreiro.com>
2016-04-09 00:55:36 +0200

    Removed garbage folder and added medium

---
+++ medium.py
@@ -0,0 +1,18 @@
+# filename = 'Data/ejemplo_entrega.txt'
+# filename = 'adults_output.csv'
+# txt = open(filename)
+#
+# total = 0.0
+# ceroes = 0.0
+# ones = 0.0
+#
+# for char in txt:
+#     char = int(char)
+#     if char == " " or char == "":
+#         pass
+#     elif char == 1:
+#         ones += 1.0
+#     total += 1.00
+#
+# accuracy = float(ones/total) * 100
+# print accuracy
